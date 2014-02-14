853fbf61d30944e0b219cfdf77b31b1fff7809eb
Dom Kiva-Meyer <git@domkm.com>
2014-02-13 20:06:49 -0800

    refactor markov and scale n-gram scoring

--- src/domkm/wug/markov.clj
+++ src/domkm/wug/markov.clj
@@ -3,40 +3,42 @@
   (:require [clojure.core :as clj]
             [clojure.string :as str]))
 
-(def n-gram-range (range 1 4))
+(def ^:private words
+  (->> (slurp "resources/words")
+       str/split-lines
+       (filter #(clj/= % (str/lower-case %)))
+       set))
 
-(defn n-grams
-  ([s] (mapcat #(n-grams % s) n-gram-range))
+(defn ^:private n-grams
+  ([s] (mapcat #(n-grams % s) (range 1 (inc (count s)))))
   ([n s] (map #(apply str %)
               (partition n 1 s))))
 
-(def n-gram-frequencies
-  (let [words (->> (slurp "resources/words")
-                   str/split-lines
-                   (filter #(clj/= % (str/lower-case %)))
-                   set)]
-    (frequencies (mapcat n-grams words))))
+(def ^:private n-gram-frequencies
+  (frequencies (mapcat n-grams words)))
 
-(def n-gram-sums
+(def ^:private n-gram-sums
   (reduce-kv (fn [coll k v]
                (let [n (count k)]
-                 (assoc coll n (+ v (coll n)))))
-             (zipmap n-gram-range (repeat 0))
+                 (assoc coll n (+ v (coll n 0)))))
+             {}
              n-gram-frequencies))
 
-(defn n-gram-probability [n-gram]
+(defn ^:private n-gram-probability [n-gram]
   (/ (n-gram-frequencies n-gram 0)
      (n-gram-sums (count n-gram))))
 
-(defn word-probability [word]
+(defn ^:private score [word]
   (->> (n-grams word)
-       (map n-gram-probability)
+       ; TODO: What is the optimal length-based scaling for n-grams?
+       ; This scales by twice the length of the n-gram.
+       (map #(* 2 (count %) (n-gram-probability %)))
        (reduce *)))
 
 (defn ^:private def-comparator-fn [sym]
   `(defn ~sym [& args#]
      (apply ~(symbol "clojure.core" (name sym))
-            (map word-probability args#))))
+            (map score args#))))
 
 (defmacro ^:private def-comparator-fns []
   `(do
